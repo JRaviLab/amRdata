179781287822a2a1ac082b984c3e9b368f5f206d
Abhirupa Ghosh <100681585+AbhirupaGhosh@users.noreply.github.com>
2026-09-09 11:43:07 -0600

    Update feature expression to replace '-' with '.'

--- R/feature_to_head.R
+++ R/feature_to_head.R
@@ -444,7 +444,7 @@ buildDyadFeatureMap <- function(
       view_name = "v_defensecas",
       parquet_dir = parquet_dir,
       dataset_name = "protein_DefenseCas",
-      feature_expr = "query_name"
+      feature_expr = "REPLACE(query_name, '-', '.')"
     )
   }
 
